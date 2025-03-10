209b5f2c5e481129dd9020eee8852a9541dc3de7
Devco01 <pepsiman15.1@gmail.com>
2025-03-10 12:58:59 +0100

    Simplification de l'API pour le déploiement sur Vercel

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,9 @@
-import { startServer } from './app';
+import { app, startServer } from './app';
 
-// Démarrer le serveur
-startServer(); 
+// Démarrer le serveur en mode non-serverless
+if (process.env.NODE_ENV !== 'production') {
+  startServer();
+}
+
+// Exporter l'application pour Vercel
+export default app; 
